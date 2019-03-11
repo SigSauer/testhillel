0bee937b2fefa6990b030609b5ecaee54fd66a79
SigSauer <dane2747@gmail.com>
2019-03-11 14:54:39 +0200

    add task info

--- src/main/java/task3/SynchronizingIncrement.java
+++ src/main/java/task3/SynchronizingIncrement.java
@@ -3,6 +3,11 @@ package task3;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SynchronizingIncrement {
+
+    /**
+     * Task: Write three different method options for getNextValue() that will return
+     *          'value++' each method needs to be synchronized in a different way.
+     */
     private int value = 0;
 
     public synchronized void firstWay() {
